05843a8ad39487f485eb55f93bb027cbdb3f638d
Marcus Miles <marcuspmiles@gmail.com>
2021-10-04 13:53:59 -0700

    Create lab1.sh

---
+++ lab1.sh
@@ -0,0 +1,8 @@
+# Script to update the OS
+# Using windows or a linux distribution, you can set certain computers to run this script at a certain date and time
+
+sudo apt-get update
+sudo apt-get upgrade      -y
+sudo apt-get dist-upgrade -y
+sudo apt-get auto-remove  -y
+sudo apt-get auto-clean   -y
